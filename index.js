234eed04a106adbcccede47731d43761dfbe8bb0
Nathaniel Miller <dukeoflaser@gmail.com>
2017-09-12 17:42:45 -0700

    Implement request methods for retrieving JSON data from API

---
+++ index.js
@@ -0,0 +1,52 @@
+const http = require('http');
+
+function getJSON(path) {
+  return http.get({
+    host: 'codetest.kube.getswift.co',
+    path: path
+    }, (res) => {
+    const { statusCode } = res;
+    const contentType = res.headers['content-type'];
+  
+    let error;
+    if (statusCode !== 200) {
+      error = new Error('Request Failed.\n' +
+                        `Status Code: ${statusCode}`);
+    } else if (!/^application\/json/.test(contentType)) {
+      error = new Error('Invalid content-type.\n' +
+                        `Expected application/json but received ${contentType}`);
+    }
+    if (error) {
+      console.error(error.message);
+      // consume response data to free up memory
+      res.resume();
+      return;
+    }
+  
+    res.setEncoding('utf8');
+    let rawData = '';
+    res.on('data', (chunk) => { rawData += chunk; });
+    res.on('end', () => {
+      try {
+        const parsedData = JSON.parse(rawData);
+        console.log(parsedData);
+      } catch (e) {
+        console.error(e.message);
+      }
+    });
+  }).on('error', (e) => {
+    console.error(`Got error: ${e.message}`);
+  });
+}
+
+
+
+const makeRequest = async (path) => {
+  return (await getJSON(path));
+}
+
+let drones = makeRequest('/drones');
+let packages = makeRequest('/packages');
+
+console.log(drones);
+console.log(packages);
