68da8433883c54f5823c2d486119a471844790f7
Nathaniel Miller <dukeoflaser@gmail.com>
2017-09-13 16:48:51 -0700

    Add functions various coordinate functions.

--- dispatcher.js
+++ dispatcher.js
@@ -1,8 +1,37 @@
 const data = require('./index.js');
+
 const drones = data.drones;
 const packages = data.packages;
-const base = { latitude: -37.816664, longitude: 144.9638476 }; //https://www.maps.ie/coordinates.html
+
+const depot = new Coords(-37.816664, 144.9638476 ); //https://www.maps.ie/coordinates.html
 const currentTime = Math.round(new Date() / 1000);
+
+
+function packageDestination(package) {
+  let lat = package["destination"]["latitude"];
+  let long = package["destination"]["longitude"];
+
+  return new Coords(lat, long);
+}
+
+function droneDestination(drone) {
+  let lat = drone["destination"]["latitude"];
+  let long = drone["destination"]["longitude"];
+
+  return new Coords(lat, long);
+}
+
+function droneLocation(drone) {
+  let lat = drone["location"]["latitude"];
+  let long = drone["location"]["longitude"];
+
+  return new Coords(lat, long);
+}
+
+function Coords(lat, long) {
+  this.latitude = lat;
+  this.longitude = long;
+}
 
 console.log(drones);
 console.log(packages);
