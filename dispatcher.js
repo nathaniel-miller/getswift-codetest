5bc0574fb3bb7340ecd877e85fb23c49ce3bf8e6
Nathaniel Miller <dukeoflaser@gmail.com>
2017-09-14 21:55:51 -0700

    Finished inital script. Output looks feasible.

--- dispatcher.js
+++ dispatcher.js
@@ -1,62 +1,44 @@
 const fetchData = require('./fetchData.js');
-// const sort = require('./sorting.js');
 const distance = require('./haversine.js'); //meters
 
 const depot = new Coords(-37.816664, 144.9638476 ); //https://www.maps.ie/coordinates.html
 const currentTime = Math.round(new Date() / 1000);
 const droneSpeed = mPerSecond(50); //enter km/hour
 
+
+function dispatch(data) {
+  const packages = sortPackages(data["packageData"]);
+  const drones = sortDrones(data["droneData"]);
+
+  let nextReturnTime = drones[0].returnTime;
+  let assignments = [];
+  let unassignedPackageIds = [];
+
+  packages.forEach(package => {
+    let requiredTime = deliveryTime(package) + nextReturnTime;
+
+    if(isDeliverable(requiredTime, availableTime(package))) {
+      assignments.push({
+        "droneId" : drones.shift().droneId,
+        "packageId" : package.packageId
+      });
+
+      nextReturnTime = drones[0].returnTime;
+    } else {
+      unassignedPackageIds.push(package.packageId);
+    }
+    
+  });
+
+  return {
+    "assignments" : assignments,
+    "unassignedPackageIds" : unassignedPackageIds
+  };
+}
+
 function mPerSecond(kmPerHour) {
   return (kmPerHour / 3600) * 1000; 
 }
-
-function dispatch(data) {
-  const drones = data["droneData"];
-  const packages = data["packageData"];
-
-  processPackages(packages);
-  processDrones(drones);
-}
-
-function processPackages(packages) {
-  packages = sortPackages(packages);
-
-
-  packages = packages.forEach(package => {
-
-    let delTime = deliveryTime(package);
-    let availTime = availableTime(package);
-
-  });
-}
-
-function processDrones(drones) {
-  drones = sortDrones(drones);
-
-}
-
-
-function packageDestination(package) {
-  let lat = package["destination"]["latitude"];
-  let long = package["destination"]["longitude"];
-
-  return new Coords(lat, long);
-}
-
-function droneDestination(drone) {
-  let lat = drone["destination"]["latitude"];
-  let long = drone["destination"]["longitude"];
-
-  return new Coords(lat, long);
-}
-
-function droneLocation(drone) {
-  let lat = drone["location"]["latitude"];
-  let long = drone["location"]["longitude"];
-
-  return new Coords(lat, long);
-}
-
 
 function timeInSeconds(distance, speed) {
   return Math.round(distance / speed);
@@ -75,10 +57,6 @@ function deliveryTime(package) {
 
 function availableTime(package) {
   return package.deadline - currentTime;
-}
-
-function requiredTime(deliveryTime, returnTime) {
-  return deliveryTime + returnTime;
 }
 
 function returnTime(drone) {
@@ -125,18 +103,6 @@ function isDeliverable(requiredTime, availableTime) {
   return availableTime - requiredTime >= 0 ? true : false;
 }
 
-
-function Coords(lat, long) {
-  this.latitude = lat;
-  this.longitude = long;
-}
-
-fetchData()
-.then(data => dispatch(data))
-.catch(reason => console.log(reason.message));
-
-
-
 function sortPackages(packages) {
   
   if(packages.length < 2) {
@@ -159,7 +125,6 @@ function sortPackages(packages) {
   return sortPackages(earlier).concat(pivot, sortPackages(later));
 }
 
-
 function sortDrones(drones) {
   if(drones.length < 2) {
     return drones;
@@ -171,8 +136,10 @@ function sortDrones(drones) {
 
   for(let i = 1; i < drones.length; i++) {
     let currentDrone = drones[i];
+    let seconds = returnTime(currentDrone);
 
-    if(returnTime(currentDrone) < pivot) {
+    if(seconds < pivot) {
+      noteReturnTime(currentDrone, seconds);
       earlier.push(currentDrone);
     } else {
       later.push(currentDrone);
@@ -181,3 +148,17 @@ function sortDrones(drones) {
 
   return sortDrones(earlier).concat(drones[0], sortDrones(later));
 }
+
+function noteReturnTime(drone, seconds) {
+  drone["returnTime"] = seconds;
+  return drone;
+}
+
+function Coords(lat, long) {
+  this.latitude = lat;
+  this.longitude = long;
+}
+
+fetchData()
+.then(data => console.log(dispatch(data)))
+.catch(reason => console.log(reason.message));
