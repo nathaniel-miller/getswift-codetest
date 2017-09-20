ea3a97a3d1fd3a167f01303f0885e49ccf8782ba
Nathaniel Miller <dukeoflaser@gmail.com>
2017-09-20 14:31:54 -0700

    Add mock data for tests to use

---
+++ test/testData.js
@@ -0,0 +1,336 @@
+module.exports = {
+  "droneData" : [
+    {
+      "droneId": 0000,
+      "location": {
+        "latitude": -37.816664, 
+        "longitude": 144.9638476
+      },
+      "packages": []
+    },
+    {
+      "droneId": 0001,
+      "location": {
+        "latitude": -38.2664,
+        "longitude": 144.9638476
+    },
+      "packages": [
+        {
+          "destination": {
+            "latitude": -37.816664, 
+            "longitude": 144.9638476
+          },
+          "packageId": 7645,
+          "deadline": 1500422916
+        }
+      ]
+    },
+    {
+      "droneId": 0002,
+      "location": {
+        "latitude": -38.2664,
+        "longitude": 144.9638476
+      },
+      "packages": []
+    },
+    {
+      "droneId": 0003,
+      "location": {
+        "latitude": -38.2664,
+        "longitude": 144.9638476
+    },
+      "packages": [
+        {
+          "destination": {
+            "latitude": -38.4912,
+            "longitude": 144.9638476
+          },
+          "packageId": 7645,
+          "deadline": 1500422916
+        }
+      ]
+    },
+    {
+      "droneId": 0004,
+      "location": {
+        "latitude": -38.2664,
+        "longitude": 144.9638476
+    },
+      "packages": [
+        {
+          "destination": {
+            "latitude": -39.4912,
+            "longitude": 146.3191586
+          },
+          "packageId": 7645,
+          "deadline": 1500422916
+        }
+      ]
+    }    
+  ],
+  "packageData" : [
+    {
+      "destination": {
+          "latitude": -37.816664,
+          "longitude": 144.9638476
+      },
+      "packageId": 0000,
+      "deadline": 1505586146
+    },
+    {
+      "destination": {
+          "latitude": -38.2664,
+          "longitude": 144.9638476
+      },
+      "packageId": 0001,
+      "deadline": 1505585146
+    },
+    {
+      "destination": {
+          "latitude": -37.816664,
+          "longitude": 144.9638476
+      },
+      "packageId": 0002,
+      "deadline": 1505584146
+    },         
+    {
+      "destination": {
+          "latitude": -37.78404125474984,
+          "longitude": 144.85238118232522
+      },
+      "packageId": 0003,
+      "deadline": 1500425202
+    },
+    {
+      "destination": {
+          "latitude": -37.77058198385452,
+          "longitude": 144.85157121265505
+      },
+      "packageId": 0004,
+      "deadline": 1500423287
+    }
+  ],
+  "droneLocations": [
+    {
+      "droneId": 0000,
+      "location": {
+        "latitude": -41.816664, 
+        "longitude": 144.9638476
+      },
+      "packages": []
+    },
+    {
+      "droneId": 0001,
+      "location": {
+        "latitude": -40.816664, 
+        "longitude": 144.9638476
+      },
+      "packages": []
+    },
+    {
+      "droneId": 0002,
+      "location": {
+        "latitude": -39.816664, 
+        "longitude": 144.9638476
+      },
+      "packages": []
+    },
+    {
+      "droneId": 0003,
+      "location": {
+        "latitude": -38.816664, 
+        "longitude": 144.9638476
+      },
+      "packages": []
+    },
+    {
+      "droneId": 0004,
+      "location": {
+        "latitude": -37.816664, 
+        "longitude": 144.9638476
+      },
+      "packages": []
+    },
+  ],
+  "droneLocationsPackages": [
+    {
+      "droneId": 0000,
+      "location": {
+        "latitude": -41.816664, 
+        "longitude": 144.9638476
+      },
+      "packages": []
+    },
+    {
+      "droneId": 0001,
+      "location": {
+        "latitude": -40.816664, 
+        "longitude": 144.9638476
+      },
+      "packages": []
+    },
+    {
+      "droneId": 0002,
+      "location": {
+        "latitude": -39.816664, 
+        "longitude": 144.9638476
+      },
+      "packages": []
+    },
+    {
+      "droneId": 0003,
+      "location": {
+        "latitude": -38.816664, 
+        "longitude": 144.9638476
+      },
+      "packages": []
+    },
+    {
+      "droneId": 0004,
+      "location": {
+        "latitude": -37.816664, 
+        "longitude": 144.9638476
+      },
+      "packages": [
+        {
+          "destination": {
+            "latitude": -38.4912,
+            "longitude": 154.9638476
+          },
+          "packageId": 7645,
+          "deadline": 1500422916
+        }
+      ]
+    },
+  ],
+  "dispatchData": [
+    {
+      "droneData": [{
+        "droneId": 0,
+        "location": {
+          "latitude": -38.816664, 
+          "longitude": 146.9638476
+        },
+        "packages": []
+      }],
+      "packageData": [{
+        "destination": {
+            "latitude": -34.816664,
+            "longitude": 148.9638476
+        },
+        "packageId": 0,
+        "deadline": 1505585146 //same as currentTime;
+      }]
+    },
+    {
+      "droneData": [{
+        "droneId": 1,
+        "location": {
+          "latitude": -37.816664, 
+          "longitude": 144.9638476
+        },
+        "packages": []
+      }],
+      "packageData": [{
+        "destination": {
+            "latitude": -38.816664,
+            "longitude": 144.9638476
+        },
+        "packageId": 1,
+        "deadline": 1605585146
+      }]
+    },
+    {
+      "droneData": [{
+        "droneId": 2,
+        "location": {
+          "latitude": -37.816664, 
+          "longitude": 144.9638476
+        },
+        "packages": []
+      }],
+      "packageData": [{
+        "destination": {
+            "latitude": -37.816664,
+            "longitude": 144.9638476
+        },
+        "packageId": 2,
+        "deadline": 1605585146
+      }]
+    },
+    {
+      "droneData": [{
+        "droneId": '3A',
+        "location": {
+          "latitude": -40.816664, 
+          "longitude": 145.9638476
+        },
+        "packages": []
+      },
+      {
+        "droneId": '3B',
+        "location": {
+          "latitude": -38.816664, 
+          "longitude": 144.9638476
+        },
+        "packages": []
+      }],
+      "packageData": [{
+        "destination": {
+            "latitude": -39.816664,
+            "longitude": 146.9638476
+        },
+        "packageId": 3,
+        "deadline": 1605585146
+      }]
+    },
+    {
+      "droneData": [{
+        "droneId": 4,
+        "location": {
+          "latitude": -37.816664, 
+          "longitude": 144.9638476
+        },
+        "packages": []
+      }],
+      "packageData": [{
+        "destination": {
+            "latitude": -35.816664,
+            "longitude": 143.9638476
+        },
+        "packageId": '4A',
+        "deadline": 1605585146
+      },
+      {
+        "destination": {
+            "latitude": -36.816664,
+            "longitude": 148.9638476
+        },
+        "packageId": '4B',
+        "deadline": 1605585146
+      }]
+    },
+    {
+      "droneData": [],
+      "packageData": [{
+        "destination": {
+            "latitude": -34.816664,
+            "longitude": 148.9638476
+        },
+        "packageId": 5,
+        "deadline": 1605585146 //same as currentTime;
+      }]
+    },
+    {
+      "droneData": [{
+        "droneId": 6,
+        "location": {
+          "latitude": -38.816664, 
+          "longitude": 146.9638476
+        },
+        "packages": []
+      }],
+      "packageData": []
+    },        
+  ]
+};
